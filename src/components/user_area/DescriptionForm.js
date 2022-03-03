6aa800f0d5ed62321fb2b548f714c9afc7f87551
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2022-03-03 02:01:31 +0100

    feature: update image description

--- src/components/user_area/DescriptionForm.js
+++ src/components/user_area/DescriptionForm.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React,{useState} from 'react';
+import {  Modal, Button, Box,TextField  } from '@mui/material';
 
-export const DescriptionForm = () => {
+import '../../assets/css/DescriptionForm.css';
+
+export const DescriptionForm = ({open, setOpen, setDescription, imgDescription}) => {
+    
+    const [value, setValue] = useState();
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    const handleUpdate = () => {
+        setDescription(value);
+        setOpen(!open);
+    }
+    const handleClose = () => {
+        setOpen(!open);
+    }
+
     return (
         <Modal
                 hideBackdrop
                 open={open}
-                onClose={handleClose}
                 aria-labelledby="child-modal-title"
                 aria-describedby="child-modal-description"
             >
-            <Box className='modalBox' sx={{ width: 400 }}>
-                <h2 id="child-modal-title">Image Added to favorites successfully</h2>
-
+            <Box className='modalBoxForm' sx={{ width: 400 }}>
+                <h2 id="child-modal-title">Edit Description</h2>
+                <TextField
+                    fullWidth 
+                    id="outlined-textarea"
+                    label="Description"
+                    placeholder="Add Description"
+                    rows={6}
+                    value={value}
+                    onChange={handleChange}
+                    multiline
+                />
+                <hr color='grey'/>
+                <Button onClick={handleUpdate} variant="contained">Update Description</Button><br /><br />
                 <Button onClick={handleClose} variant="contained">Close</Button>
             </Box>
         </Modal>
--- src/components/user_area/ImageCard.js
+++ src/components/user_area/ImageCard.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import {Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, IconButton, Typography} from '@mui/material';
 import { red } from '@mui/material/colors';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import storage from '../../utils/storage/storage';
+import {DescriptionForm} from './DescriptionForm';
 
 export const ImageCard = ({img, setMyImages}) => {
+
+    
+    const [open, setOpen] = useState(false);
+    const [description, setDescription] = useState();
+
+    
+    useEffect(() => {
+        console.log('DESCRIPTION', description);
+        description && updateDescription(img, description);
+    },[description])
 
     let date = new Date(img.date);
 
@@ -14,6 +25,21 @@ export const ImageCard = ({img, setMyImages}) => {
                                     .filter(image => image.id !== imageId);
         storage.set(process.env.REACT_APP_USER_FAVORITES, updateFavorites);
         setMyImages(updateFavorites);
+    }
+
+    const openEdit = () => {
+        setOpen(!open);
+    }
+
+    const updateDescription = (img) => {
+        console.log('IMAGE', img)
+        img.description = description;
+        console.log('IMAGE 2', img)
+        deleteItem(img.id);
+        const update = storage.get(process.env.REACT_APP_USER_FAVORITES);
+        update.push(img);
+        storage.set(process.env.REACT_APP_USER_FAVORITES, update);
+        setMyImages(update);
     }
 
     return (
@@ -35,17 +61,18 @@ export const ImageCard = ({img, setMyImages}) => {
             />
             <CardContent>
                 <Typography variant="body2" color="text.secondary">
-                {img.description? img.description : (<h3>Image without description</h3>)}
+                {img.description? img.description : (<b>Image without description</b>)}
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
                 <IconButton aria-label="remove from favorites" onClick={() => deleteItem(img.id)}>
                     <DeleteIcon />
                 </IconButton>
-                <IconButton aria-label="edit">
+                <IconButton aria-label="edit" onClick={openEdit}>
                     <EditIcon />
                 </IconButton>
             </CardActions>
+        <DescriptionForm open={open} setOpen={setOpen} setDescription={setDescription} imgDescription={img.description} />
         </Card>
     );
 }
